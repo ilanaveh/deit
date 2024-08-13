48ffc91fefe546f1f25f77ea37722134c6b04c61
ilanaveh <ilana.naveh@mail.huji.ac.il>
2024-08-13 17:25:52 +0300

    change plot linestyle & add metric options

--- deit_plot_performance.py
+++ deit_plot_performance.py
@@ -18,7 +18,7 @@ def plot_metric_multiple_dirs(models, metric):
             log_data = read_log_file(filepath)
             epochs = [entry['epoch'] for entry in log_data]
             values = [entry[metric] for entry in log_data]
-            plt.plot(epochs, values, marker='.', linestyle='-', label=mdl)
+            plt.plot(epochs, values, linestyle='-', label=mdl)
         else:
             print(f"Log file not found in directory: {mdl}")
 
@@ -33,6 +33,6 @@ def plot_metric_multiple_dirs(models, metric):
 # Usage example
 if __name__ == "__main__":
     models = ['original', 'deit_blur8', 'deit_blur16']
-    metric = 'test_acc1'  # Choose: train_loss / test_acc1 / test_acc5
+    metric = 'test_acc1'  # Choose: train_loss / test_loss / test_acc1 / test_acc5 / train_lr
 
     plot_metric_multiple_dirs(models, metric)
