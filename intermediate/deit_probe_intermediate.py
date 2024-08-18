6bc7faa68e750cd051782dda001f5782fb2b75d6
ilanaveh <ilana.naveh@mail.huji.ac.il>
2024-08-18 12:45:56 +0300

    cosmetic - move main to beginning

--- intermediate/deit_probe_intermediate.py
+++ intermediate/deit_probe_intermediate.py
@@ -18,56 +18,6 @@ import argparse
 from PIL import ImageFilter
 
 
-def create_model_probed(block_ind, num_classes=2, model_path=None):
-    """
-    create model based on a pretrained deit, with a FC layer after the chosen block from deit.
-    Freeze all parameters
-    except for the new FC layer.
-    :param model_path: path to trained deit checkpoint, from which we want to start.
-    :param block_ind: number of block we want to probe from (its output) - between 0-11.
-    :param num_classes: # of output classes (2 for binary classification).
-    :return: probed model
-    """
-
-    # Create deit model with parameters according to those given in main.py:
-    model = create_model(
-        'deit_base_patch16_224',
-        pretrained=False,
-        num_classes=1000,
-        drop_rate=0,
-        drop_path_rate=0.1,
-        drop_block_rate=None,
-        img_size=224
-    )
-
-    model.cuda()
-    model.eval()
-
-    # Load trained checkpoint:
-    if model_path:
-        deit_checkpoint = torch.load(os.path.join(model_path, 'checkpoint.pth'), map_location='cpu')
-        model.load_state_dict(deit_checkpoint['model'])
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Remove all blocks downstream to chosen block: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    inds2rmv = range(len(model.blocks) - 1, block_ind, -1)
-
-    for i in inds2rmv:
-        del model.blocks[i]
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~ Replace last FC layer with a new one, for binary classification: ~~~~~~~~~~~~~~~~~~~~~~~~~
-    model.head = nn.Linear(model.embed_dim, num_classes)  # embed_dim = 768 (don't change, only change num_classes).
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Freeze weights of all layers, except for last FC: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    for param in model.parameters():
-        param.requires_grad = False
-
-    model.head.bias.requires_grad = True
-    model.head.weight.requires_grad = True
-
-    return model
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch-size', default=32, type=int, metavar='N', help='mini-batch size (default: 2)')
@@ -82,7 +32,7 @@ def main():
 
     # When debugging - change model sufix, and set normal LR:
     db_suf = '' if args.lr else '_db'
-    args.lr = args.lr if args.lr else .01
+    args.lr = args.lr if args.lr else 1e-5
 
     if args.deit_model_name:
         # Change name of loaded model, according to its blur:
@@ -231,6 +181,55 @@ def main():
 
         print(f'Epoch {ep}: Checkpoint Saved.')
 
+
+def create_model_probed(block_ind, num_classes=2, model_path=None):
+    """
+    create model based on a pretrained deit, with a FC layer after the chosen block from deit.
+    Freeze all parameters
+    except for the new FC layer.
+    :param model_path: path to trained deit checkpoint, from which we want to start.
+    :param block_ind: number of block we want to probe from (its output) - between 0-11.
+    :param num_classes: # of output classes (2 for binary classification).
+    :return: probed model
+    """
+
+    # Create deit model with parameters according to those given in main.py:
+    model = create_model(
+        'deit_base_patch16_224',
+        pretrained=False,
+        num_classes=1000,
+        drop_rate=0,
+        drop_path_rate=0.1,
+        drop_block_rate=None,
+        img_size=224
+    )
+
+    model.cuda()
+    model.eval()
+
+    # Load trained checkpoint:
+    if model_path:
+        deit_checkpoint = torch.load(os.path.join(model_path, 'checkpoint.pth'), map_location='cpu')
+        model.load_state_dict(deit_checkpoint['model'])
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Remove all blocks downstream to chosen block: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    inds2rmv = range(len(model.blocks) - 1, block_ind, -1)
+
+    for i in inds2rmv:
+        del model.blocks[i]
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~ Replace last FC layer with a new one, for binary classification: ~~~~~~~~~~~~~~~~~~~~~~~~~
+    model.head = nn.Linear(model.embed_dim, num_classes)  # embed_dim = 768 (don't change, only change num_classes).
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Freeze weights of all layers, except for last FC: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    model.head.bias.requires_grad = True
+    model.head.weight.requires_grad = True
+
+    return model
 
 def save_checkpoint(state, is_best, filedir='.'):
     filename = os.path.join(filedir, 'checkpoint.pth.tar')
