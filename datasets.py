f68ddf84c3b94db417d34e983d76e985b2b01363
ilanaveh <ilana.naveh@mail.huji.ac.il>
2024-09-29 17:32:50 +0300

    add __repr__ to CustomCompose

--- datasets.py
+++ datasets.py
@@ -309,3 +309,11 @@ class CustomCompose:
         else:
             return img
 
+    def __repr__(self) -> str:
+        format_string = self.__class__.__name__ + "("
+        for t in self.transforms:
+            format_string += "\n"
+            format_string += f"    {t}"
+        format_string += "\n)"
+        return format_string
+
