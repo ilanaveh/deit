0fdbd2bdda8529e712d1a827c461c002ca85d8b7
ilanaveh <ilana.naveh@mail.huji.ac.il>
2025-04-23 10:58:00 +0300

    Added: 1. code for plotting bar-plot of each model's best performance (doesn't    work yet) 2. splitted model list to two lists, according to where 'out' folder is. 3. code for plotting variable-blur models (currently plots performance    of highest blur-level from the range.

--- deit_plot_performance.py
+++ deit_plot_performance.py
@@ -1,12 +1,43 @@
 import json
 import os
 import matplotlib.pyplot as plt
+import torch
 
 
 def read_log_file(filepath):
     with open(filepath, 'r') as file:
         log_data = [json.loads(line) for line in file]
     return log_data
+
+
+def get_epoch_acc(log_data, epoch):
+    for epoch_data in log_data:
+        if epoch_data['epoch'] == epoch:
+            return epoch_data['test_acc1']
+
+
+# Create a function to get the appropriate color based on model name
+color_map = {
+    'blur0-32': 'cyan',
+    'blur0-16': 'pink',
+    'blur16-32': 'yellow',
+    'blur0': 'blue',
+    'original': 'blue',
+    'blur2': 'green',
+    'blur4': 'red',
+    'blur6': 'black',
+    'blur8': 'purple',
+    'blur16': 'orange',
+    'blur32': 'brown'
+
+}
+
+
+def get_color_for_model(model_name):
+    for blur_level in color_map.keys():
+        if blur_level in model_name:
+            return color_map[blur_level]
+    return 'gray'  # Default color if no match is found
 
 
 def plot_metric(models, metrics):
@@ -18,33 +49,118 @@ def plot_metric(models, metrics):
     :return:
     """
 
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(4.5, 3))
+    # Initialize a set to keep track of which blur levels have been added to the legend
+    legend_added = set()
 
     for i, metric in enumerate(metrics):
         if len(metrics) > 1:
             plt.subplot(2, 2, i + 1)
 
-        for mdl in models:
+        for mdl in models[0]:
+            filepath = os.path.join('out', 'jobs_from_scratch_main_tmp_code', mdl, 'log.txt')
+            if os.path.exists(filepath):
+                log_data = read_log_file(filepath)
+                epochs = [entry['epoch'] for entry in log_data]
+                if 'deit_blur0-32' in mdl:
+                    values = [entry[metric.replace('_', '_blur_max_')] for entry in log_data]
+                elif ('deit_blur0-16' in mdl) or ('deit_blur16-32' in mdl):
+                    blur_max = mdl.split('-')[1].split('_')[0]
+                    values = [entry[metric.replace('_', f'_blur_{blur_max}_')] for entry in log_data]
+                else:
+                    values = [entry[metric] for entry in log_data]
+                color = get_color_for_model(mdl)
+                plt.plot(epochs, values, linestyle='-', color=color)
+                blur_level = next((blur for blur in color_map.keys() if blur in mdl), None)
+                if blur_level and blur_level not in legend_added:
+                    plt.plot([], [], color=color, label=blur_level)  # Add empty plot for legend
+                    legend_added.add(blur_level)
+
+            else:
+                print(f"Log file not found in directory: {mdl}")
+
+        for mdl in models[1]:
             filepath = os.path.join('out', mdl, 'log.txt')
             if os.path.exists(filepath):
                 log_data = read_log_file(filepath)
                 epochs = [entry['epoch'] for entry in log_data]
-                values = [entry[metric] for entry in log_data]
-                plt.plot(epochs, values, linestyle='-', label=mdl)
+                if 'deit_blur0-32' in mdl:
+                    values = [entry[metric.replace('_', '_blur_max_')] for entry in log_data]
+                elif ('deit_blur0-16' in mdl) or ('deit_blur16-32' in mdl):
+                    blur_max = mdl.split('-')[1].split('_')[0]
+                    values = [entry[metric.replace('_', f'_blur_{blur_max}_')] for entry in log_data]
+                else:
+                    values = [entry[metric] for entry in log_data]
+                color = get_color_for_model(mdl)
+                plt.plot(epochs, values, linestyle='-', color=color)
+                if 'original' in mdl:
+                    blur_level = 'blur0'
+                else:
+                    blur_level = next((blur for blur in color_map.keys() if blur in mdl), None)
+                if blur_level and blur_level not in legend_added:
+                    plt.plot([], [], color=color, label=blur_level)  # Add empty plot for legend
+                    legend_added.add(blur_level)
             else:
                 print(f"Log file not found in directory: {mdl}")
 
-        plt.title(metric)
         plt.xlabel('Epoch')
-        plt.ylabel(metric)
+        if metric == 'test_acc1':
+            plt.ylabel('Top1 Accuracy')
         plt.grid(True)
+        ax = plt.gca()
+        ax.set_position([.125, .15, .8, .8])
+
     plt.legend(title='Model')
-    plt.show()
+
+
+def plot_bars(models):
+    """
+    plot accuracy of the best checkpoint of each model (not entire progression during training as in 'plot_metric').
+    """
+
+    f = plt.figure(figsize=(4.5, 2.6))
+
+    best_accs = {mdl: 0 for mdl in models}
+
+    for mdl in models:
+        filepath = os.path.join('out', mdl, 'log.txt')
+        best_cp_pth = os.path.join('out', mdl, 'best_checkpoint.pth')
+        if os.path.exists(filepath):
+            log_data = read_log_file(filepath)
+            best_cp = torch.load(best_cp_pth)
+            best_epoch = best_cp['epoch']
+            best_accs[mdl] = get_epoch_acc(log_data, best_epoch)
+        else:
+            print(f"Log file not found in directory: {mdl}")
+
+    bars = plt.bar(best_accs.keys(), best_accs.values(), zorder=3)
+    for i, bar in enumerate(bars):
+        bar.set_color(plt.rcParams['axes.prop_cycle'].by_key()['color'][
+                          i % len(plt.rcParams['axes.prop_cycle'].by_key()['color'])])
+
+    plt.title('Validation Performance')
+    plt.ylabel('Top1 Accuracy')
+    plt.grid(axis='y', zorder=0)
+    plt.ylim([0, 100])
 
 
 if __name__ == "__main__":
     # , 'deit_8gpu', 'deit_blur4', 'deit_blur32'
-    models = ['original', 'deit_blur8', 'deit_blur16', 'deit_blur32']
+    # models = ['original', 'deit_blur4', 'deit_blur4_rep', 'deit_blur8', 'deit_blur16', 'deit_blur32', 'deit_blur0-32',
+    #           'deit_blur0-32_tmp', 'deit_blur0_rep']
+    # , 'deit_blur0-32_rep'
+
+    # models is two lists: models[0] - from 'out/jobs_from_scratch_main_tmp_code'; models[1] - from 'out'.
+    models = [
+        ['deit_blur0_tmp_new', 'deit_blur2_tmp_new', 'deit_blur4_tmp_new', 'deit_blur6_tmp_new', 'deit_blur8_tmp_new',
+         'deit_blur32_tmp_new', 'deit_blur0-32_tmp_new', 'deit_blur6_rep', 'deit_blur8_rep', 'deit_blur16_tmp_new',
+         'deit_blur0-16_tmp', 'deit_blur16-32_tmp'],
+        ['original', 'deit_blur4', 'deit_blur8', 'deit_blur16', 'deit_blur32', 'deit_blur0-32_tmp', 'deit_blur4_rep']]
+    # models = [
+    #     ['deit_blur0_tmp_new', 'deit_blur4_tmp_new', 'deit_blur8_rep',
+    #      'deit_blur32_tmp_new', 'deit_blur0-32_tmp_new', 'deit_blur16_tmp_new'],
+    #     []]
     metric = ['test_acc1']  # Choose: train_loss / test_loss / test_acc1 / test_acc5 / train_lr
     # metrics = ['train_loss', 'test_loss', 'train_lr', 'test_acc1']
+    # plot_bars(models)
     plot_metric(models, metric)
