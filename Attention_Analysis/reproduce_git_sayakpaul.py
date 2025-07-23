4797452be2c2cb263c92d4e6294df4a64e566544
ilanaveh <ilana.naveh@mail.huji.ac.il>
2025-07-23 14:30:40 +0300

    Change 'blur' to 'inp_blur' and add option for multiple models

--- Attention_Analysis/reproduce_git_sayakpaul.py
+++ Attention_Analysis/reproduce_git_sayakpaul.py
@@ -17,10 +17,6 @@ averaging over heads).
     added this option here as well.
 
 """
-
-import timm
-import torch
-
 import numpy as np
 import os
 import os.path as osp
@@ -29,34 +25,46 @@ import torch
 import matplotlib.pyplot as plt
 import pickle
 import json
-
+from attention_distances import model_out_dict, color_map, get_color_for_model
 
 save_fig = False
 
 layer_indices = np.arange(12)
 head_indices = np.arange(12)
-model_names = ['original']
 sort_heads = True
-
-blur = 0  # input blur
-n_patches = 14  # property of deit (14 patches in each row/column -> total 196 patches).
-patch_size = 16
-
+inp_blur = 0  # input blur
+limit_n_ims = 10
 attn_layers_to_show = [3, 4, 10, 11]  # List of indices between 0-11. In git plot: [3, 4, 10, 11].
 # attn_layers_to_show = np.arange(12)
 
-lyr_color_dict = {0: 'yellow',
-                  1: 'orange',
-                  2: 'olive',
-                  3: 'blue',
-                  4: 'green',
+model_names = ['pretrained', 'deit_blur0_tmp_new', 'deit_blur16_tmp_new', 'deit_blur0-16_tmp_fix_bug']
+# See attention_distances for full list of models (in model_out_dict)
+
+n_patches = 14  # property of deit (14 patches in each row/column -> total 196 patches).
+patch_size = 16
+
+mdl_label_dict = {
+    'pretrained': 'pretrained',
+    'deit_blur0_tmp_new': 'high-res',
+    'deit_blur16_tmp_new': 'Uniform blur (16)',
+    'deit_blur0-16_tmp_fix_bug': 'Variable blur (0-16)'
+}
+
+# Add 'pretrained' to color_map:
+color_map['pretrained'] = 'magenta'
+
+lyr_color_dict = {0: 'cyan',
+                  1: 'blue',
+                  2: 'green',
+                  3: 'olive',
+                  4: 'grey',
                   5: 'black',
-                  6: 'purple',
-                  7: 'brown',
+                  6: 'brown',
+                  7: 'purple',
                   8: 'pink',
-                  9: 'grey',
-                  10: 'red',
-                  11: 'cyan'}
+                  9: 'red',
+                  10: 'orange',
+                  11: 'yellow'}
 
 # Get Imagenet info:
 with open('imagenet1000_clsidx_to_labels.txt', 'r') as file:
@@ -82,78 +90,94 @@ fig_name = fig_name.replace('.png', '_sorted.png') if sort_heads else fig_name
 
 
 def main():
+    # for each model, layer and head - a list of attention-maps for all images (each attnetion-map includes all tokens):
+    all_attn = {mdl: {layer: {head: [] for head in head_indices}
+                      for layer in layer_indices}
+                for mdl in model_names}
 
-    # for each layer and head - a list of attention-maps for all images (each attnetion-map includes all tokens):
-    all_attn = {layer: {head: [] for head in head_indices} for layer in layer_indices}
+    # for each model, layer and head - a list of distances for all images:
+    all_distances = {mdl: {layer: {head: [] for head in head_indices}
+                           for layer in layer_indices}
+                     for mdl in model_names}
 
-    # for each layer and head - a list of distances for all images:
-    all_distances = {layer: {head: [] for head in head_indices} for layer in layer_indices}
+    for mdl in model_names:
+        print(mdl)
 
-    # Get attention maps, for each layer:
-    # -------------------------------
-    # 1. Load deit model:
-    # -------------------------------
-    model = get_model_with_attn()  # don't pass model_path, so by default original (pretrained) deit model is loaded.
-
-    # -------------------------------
-    # 2. Load and preprocess image:
-    # -------------------------------
-
-    for (i, img_name) in enumerate(os.listdir(osp.join(img_pth, img_dataset, img_cat))):
-        if img_name == 'Thumbs.db':
-            continue
-        print(f"image {i}")
-        img_full_pth = osp.join(img_pth, img_dataset, img_cat, img_name)
-        original_image, input_tensor = load_and_preprocess_img(img_full_pth, blur, show_im_with_blur=False)
+        # Get attention maps, for each layer:
+        # -------------------------------
+        # 1. Load deit model:
+        # -------------------------------
+        if mdl == 'pretrained':
+            # don't pass model_path, so by default original (pretrained) deit model is loaded.
+            model = get_model_with_attn()
+        else:
+            model_path = osp.join('/home/projects/bagon/ilanaveh/code/Transformers/deit/', model_out_dict[mdl], mdl)
+            model = get_model_with_attn(model_path)
 
         # -------------------------------
-        # 3. Forward Pass
+        # 2. Load and preprocess image:
         # -------------------------------
-        with torch.no_grad():
-            out = model(input_tensor)
 
-        for lyr in layer_indices:
-            # get attention for each layer, do not average over heads:
-            lyr_attn = np.squeeze(model.blocks[lyr].attn.last_attn)  # [nheads=12, ntokens=197, ntokens=197]
+        for (i, img_name) in enumerate(os.listdir(osp.join(img_pth, img_dataset, img_cat))):
+            if i >= limit_n_ims:
+                break
 
-            # Remove [CLS] token (which isn't relevant for distances:
-            lyr_attn_no_CLS = lyr_attn[:, 1:, 1:]  # [nheads=12, ntokens=196, ntokens=196]
+            if img_name == 'Thumbs.db':
+                continue
+            print(f"image {i}")
+            img_full_pth = osp.join(img_pth, img_dataset, img_cat, img_name)
+            original_image, input_tensor = load_and_preprocess_img(img_full_pth, inp_blur, show_im_with_blur=False)
 
-            # Insert to dictionary:
-            for h in head_indices:
-                cur_head_attn = lyr_attn_no_CLS[h]  # [ntokens=196, ntokens=196]
+            # -------------------------------
+            # 3. Forward Pass
+            # -------------------------------
+            with torch.no_grad():
+                out = model(input_tensor)
 
-                all_attn[lyr][h] = cur_head_attn
+            for lyr in layer_indices:
+                # get attention for each layer, do not average over heads:
+                lyr_attn = np.squeeze(model.blocks[lyr].attn.last_attn)  # [nheads=12, ntokens=197, ntokens=197]
 
-                # Initialize a list to store all scaled distances for layer and head (one item for each token)
-                all_scaled_distances = []
+                # Remove [CLS] token (which isn't relevant for distances:
+                lyr_attn_no_CLS = lyr_attn[:, 1:, 1:]  # [nheads=12, ntokens=196, ntokens=196]
 
-                for token_idx in range(cur_head_attn.shape[0]):  # 0-195 (0 is not [CLS] token, since it was removed)
-                    token_attn = cur_head_attn[token_idx, :]
+                # Insert to dictionary:
+                for h in head_indices:
+                    cur_head_attn = lyr_attn_no_CLS[h]  # [ntokens=196, ntokens=196]
 
-                    # Renormalize (normalization was lost, since we removed [CLS] token):
-                    token_attn = token_attn / token_attn.sum()
-                    assert np.round(token_attn.sum(), 3) == 1
+                    all_attn[mdl][lyr][h] = cur_head_attn
 
-                    # Get attention grid of token. Shape: [14, 14]
-                    token_attn_grid = token_attn.reshape(n_patches, n_patches).detach().cpu().numpy()
+                    # Initialize a list to store all scaled distances for layer and head (one item for each token)
+                    all_scaled_distances = []
 
-                    patch_x, patch_y = divmod(token_idx, n_patches)
-                    grid_x, grid_y = torch.meshgrid(torch.arange(n_patches), torch.arange(n_patches), indexing='ij')
-                    distances = torch.sqrt((grid_x - patch_x) ** 2 +
-                                           (grid_y - patch_y) ** 2)
+                    for token_idx in range(
+                            cur_head_attn.shape[0]):  # 0-195 (0 is not [CLS] token, since it was removed)
+                        token_attn = cur_head_attn[token_idx, :]
 
-                    distances = distances * patch_size
+                        # Renormalize (normalization was lost, since we removed [CLS] token):
+                        token_attn = token_attn / token_attn.sum()
+                        assert np.round(token_attn.sum(), 3) == 1
 
-                    # Scale distances by the pixel values in the map
-                    scaled_distances = distances * token_attn_grid
+                        # Get attention grid of token. Shape: [14, 14]
+                        token_attn_grid = token_attn.reshape(n_patches, n_patches).detach().cpu().numpy()
 
-                    # Sum the scaled distances and add to list
-                    all_scaled_distances.append(scaled_distances.sum())  # mean over tokens (use sum since we had softmax)
+                        patch_x, patch_y = divmod(token_idx, n_patches)
+                        grid_x, grid_y = torch.meshgrid(torch.arange(n_patches), torch.arange(n_patches), indexing='ij')
+                        distances = torch.sqrt((grid_x - patch_x) ** 2 +
+                                               (grid_y - patch_y) ** 2)
 
-                # Compute the average scaled distance
-                avg_scaled_distance = np.mean(all_scaled_distances)
-                all_distances[lyr][h].append(avg_scaled_distance)
+                        distances = distances * patch_size
+
+                        # Scale distances by the pixel values in the map
+                        scaled_distances = distances * token_attn_grid
+
+                        # Sum the scaled distances and add to list
+                        all_scaled_distances.append(
+                            scaled_distances.sum())  # mean over tokens (use sum since we had softmax)
+
+                    # Compute the average scaled distance
+                    avg_scaled_distance = np.mean(all_scaled_distances)
+                    all_distances[mdl][lyr][h].append(avg_scaled_distance)
 
     # plot:
     fig = plt.figure()
@@ -168,7 +192,7 @@ def main():
     plt.xticks(head_indices)
     plt.xlabel('Attention Heads')
     plt.ylabel('Mean Attention Distance (px)')
-    plt.title(f"{img_cat} ({img_lbl}), {len(lyr_dists[h])} images\n(model: deit pretrained)")
+    plt.title(f"{img_cat} ({img_lbl}), {len(lyr_dists[h])} images\nInput Blur: {inp_blur}")
     plt.legend()
     fig = plt.gcf()
     fig.set_size_inches([7.2, 4.75])
@@ -182,5 +206,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
