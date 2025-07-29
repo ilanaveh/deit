e01582227b1eda3afcf2ac423be722b60ebeb5a8
ilanaveh <ilana.naveh@mail.huji.ac.il>
2025-07-29 13:17:33 +0300

    Add option for blur-range in lora training, using args.blur_max (based on deit/main_tmp.py)

--- LoRA/main_lora.py
+++ LoRA/main_lora.py
@@ -26,7 +26,7 @@ from timm.utils import NativeScaler, get_state_dict, ModelEma
 from timm.optim import create_optimizer
 from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
 from timm.scheduler import create_scheduler
-from deit.datasets import build_dataset, add_blur_transform
+from deit.datasets import build_dataset, add_blur_transform, build_dataset_blur
 from Code_from_Liel.modeling_finetune import inject_lora_vit
 import loralib as lora
 import time
@@ -34,6 +34,7 @@ from deit.engine import train_one_epoch, evaluate
 import json
 import datetime
 from tensorboardX import SummaryWriter
+from collections import Counter
 
 
 def get_args_parser():
@@ -239,7 +240,7 @@ def main(args):
     output_dir.mkdir(parents=False, exist_ok=True)  # create output_dir if doesn't exist, alert if parent doesn't exist.
 
     print(f"Creating dataset: {args.data_set}")
-    dataset_train, args.nb_classes = build_dataset(is_train=True, args=args)
+    dataset_train, args.nb_classes = build_dataset_blur(is_train=True, args=args, return_blur=bool(args.blur_max))
     dataset_val, _ = build_dataset(is_train=False, args=args)
 
     if args.distributed:
@@ -270,10 +271,43 @@ def main(args):
         drop_last=False
     )
 
-    if args.blur:
-        # Add blur transform to train & val dataloaders (single blur):
-        data_loader_train.dataset.transform = add_blur_transform(data_loader_train.dataset.transform, args.blur)
-        data_loader_val.dataset.transform = add_blur_transform(data_loader_val.dataset.transform, args.blur)
+    if args.blur_max:
+        # 1. Add blur transform to train dataloader, with range of blurs:
+        data_loader_train.dataset.transform = \
+            add_blur_transform(data_loader_train.dataset.transform, args.blur, blur_max=args.blur_max,
+                               use_custom_compose=True)
+
+        # 2. Create val dataloaders for each of the blurs in range:
+        datasets_val_blurs = {b: build_dataset(is_train=False, args=args)[0]
+                              for b in range(args.blur, args.blur_max + 1)}
+
+        dataloaders_val_blurs = {b:
+            torch.utils.data.DataLoader(
+                datasets_val_blurs[b], sampler=sampler_val,
+                batch_size=int(1.5 * args.batch_size),
+                num_workers=args.num_workers,
+                pin_memory=args.pin_mem,
+                drop_last=False
+            ) for b in range(args.blur, args.blur_max + 1)}
+
+        for b in range(args.blur, args.blur_max + 1):
+            dataloaders_val_blurs[b].dataset.transform = \
+                add_blur_transform(dataloaders_val_blurs[b].dataset.transform, b)
+
+    else:
+        # Create single val dataloader:
+        data_loader_val = torch.utils.data.DataLoader(
+            dataset_val, sampler=sampler_val,
+            batch_size=int(1.5 * args.batch_size),
+            num_workers=args.num_workers,
+            pin_memory=args.pin_mem,
+            drop_last=False
+        )
+
+        if args.blur:
+            # Add blur transform to train & val dataloaders (single blur):
+            data_loader_train.dataset.transform = add_blur_transform(data_loader_train.dataset.transform, args.blur)
+            data_loader_val.dataset.transform = add_blur_transform(data_loader_val.dataset.transform, args.blur)
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ For creating Tensorboard log: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -468,13 +502,23 @@ def main(args):
             data_loader_train.sampler.set_epoch(epoch)
 
         # ToDo: check if I need to switch to Liel's version of train_one_epoch (e.g. for dealing with LR scheduler):
-        train_stats = train_one_epoch(
-            model, criterion, data_loader_train,
-            optimizer, device, epoch, loss_scaler,
-            args.clip_grad, model_ema, mixup_fn,
-            set_training_mode=args.train_mode,
-            # keep in eval mode for deit finetuning / train mode for training and deit III finetuning
-            args=args,
+        if args.blur_max:
+            train_stats, applied_blurs_all = train_one_epoch(
+                model, criterion, data_loader_train,
+                optimizer, device, epoch, loss_scaler,
+                args.clip_grad, model_ema, mixup_fn,
+                set_training_mode=args.train_mode,
+                # keep in eval mode for deit finetuning / train mode for training and deit III finetuning
+                args=args
+            )
+        else:
+            train_stats = train_one_epoch(
+                model, criterion, data_loader_train,
+                optimizer, device, epoch, loss_scaler,
+                args.clip_grad, model_ema, mixup_fn,
+                set_training_mode=args.train_mode,
+                # keep in eval mode for deit finetuning / train mode for training and deit III finetuning
+                args=args,
         )
 
         lr_scheduler.step(epoch)
@@ -502,11 +546,25 @@ def main(args):
                         'args': args,
                     }, checkpoint_path)
 
-        test_stats = evaluate(data_loader_val, model, device)
-        print(f"Epoch {epoch} - Accuracy of the network on the {len(dataset_val)} test images with blur "
-              f"{args.blur}: {test_stats['acc1']:.1f}%")
+        if args.blur_max:
+            test_stats_blurs = {b: evaluate(dataloaders_val_blurs[b], model, device)
+                                for b in range(args.blur, args.blur_max + 1)}
 
-        current_acc = test_stats["acc1"]
+            print(f"Accuracy of the network on the {len(dataset_val)} test images with minimal blur "
+                  f"({args.blur}): {test_stats_blurs[args.blur]['acc1']:.1f}%")
+
+            print(f"Accuracy of the network on the {len(dataset_val)} test images with maximal blur "
+                  f"({args.blur_max}): {test_stats_blurs[args.blur_max]['acc1']:.1f}%")
+
+            current_acc = test_stats_blurs[args.blur]["acc1"]
+
+        else:
+            test_stats = evaluate(data_loader_val, model, device)
+
+            print(f"Epoch {epoch} - Accuracy of the network on the {len(dataset_val)} test images with blur "
+                  f"{args.blur}: {test_stats['acc1']:.1f}%")
+
+            current_acc = test_stats["acc1"]
 
         if max_accuracy < current_acc:
             max_accuracy = current_acc
@@ -535,15 +593,33 @@ def main(args):
 
         print(f'Max accuracy: {max_accuracy:.2f}%')
 
-        log_stats = {'epoch': epoch,
-                     **{f'train_{k}': v for k, v in train_stats.items()},
-                     **{f'test_{k}': v for k, v in test_stats.items()},
-                     'n_parameters': n_parameters}
+        if args.blur_max:
+            # Count amount of each blur:
+            count_blurs = Counter(applied_blurs_all)
+            count_blurs_dict = {b: count_blurs.get(b, 0) for b in range(args.blur_max + 1)}
+
+            log_stats = {'epoch': epoch,
+                         **{f'train_{k}': v for k, v in train_stats.items()},
+                         # k1 - blur level (keys in 'test_stats_blurs'); k2 - log metric (acc1, loss, etc.);
+                         # v - value of metric; [blur_dict - test_stats for current blur.]
+                         **{f'test_blur_{k1}_{k2}': v for k1, blur_dict in test_stats_blurs.items()
+                            for k2, v in blur_dict.items()},
+                         'n_parameters': n_parameters,
+                         **{f'count_blur_{k}': v for k, v in count_blurs_dict.items()}}
+        else:
+            log_stats = {'epoch': epoch,
+                         **{f'train_{k}': v for k, v in train_stats.items()},
+                         **{f'test_{k}': v for k, v in test_stats.items()},
+                         'n_parameters': n_parameters}
 
         if args.output_dir and utils.is_main_process():
             print(f"Saving epoch {epoch} stats to log file at: {output_dir}")
             with (output_dir / "log.txt").open("a") as f:
                 f.write(json.dumps(log_stats) + "\n")
+
+            if args.blur_max:
+                with (output_dir / "applied_blurs.txt").open("a") as f:
+                    f.write(f"The blurs applied in epoch {epoch}:\n{json.dumps(applied_blurs_all)}\n\n")
         else:
             print("Not saving log.")
 
