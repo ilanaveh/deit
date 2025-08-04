b3c56504b29ccacd8a0ed1651d4ad1701996cb54
ilanaveh <ilana.naveh@mail.huji.ac.il>
2025-08-04 16:31:03 +0300

    Remove pretrained_cfg from args in deit_base_distilled_patch16_224 (to fix bug)

--- models.py
+++ models.py
@@ -140,6 +140,9 @@ def deit_small_distilled_patch16_224(pretrained=False, **kwargs):
 
 @register_model
 def deit_base_distilled_patch16_224(pretrained=False, **kwargs):
+    # Filter out the 'pretrained_cfg' & 'pretrained_cfg_overlay' arguments: IN 08/25 copied from deit_base_patch16_224)
+    kwargs.pop('pretrained_cfg', None)
+    kwargs.pop('pretrained_cfg_overlay', None)
     model = DistilledVisionTransformer(
         patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
