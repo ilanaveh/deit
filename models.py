75deac2321ba12f39945969464fc3360518a10d7
ilanaveh <ilana.naveh@mail.huji.ac.il>
2025-11-06 14:41:32 +0200

    Add create function for model with mask (calls ViTMask instead of original VisionTransformer)

--- models.py
+++ models.py
@@ -8,6 +8,8 @@ from timm.models.vision_transformer import VisionTransformer, _cfg
 from timm.models.registry import register_model
 from timm.models.layers import trunc_normal_
 
+from downstream_training.module_wrappers import VitMask
+
 
 __all__ = [
     'deit_tiny_patch16_224', 'deit_small_patch16_224', 'deit_base_patch16_224',
@@ -96,6 +98,29 @@ def deit_base_patch16_224(pretrained=False, **kwargs):
     kwargs.pop('pretrained_cfg_overlay', None)
     
     model = VisionTransformer(
+        patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_base_patch16_224-b5f2ef4d.pth",
+            map_location="cpu", check_hash=True
+        )
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+
+@register_model
+def deit_base_patch16_224_mask(pretrained=False, **kwargs):
+    """
+    Same as deit_base_patch16_224, but instead of VisionTransformer, calls VitMask (modified forward, enabling to mask
+    patches).
+    """
+    # Filter out the 'pretrained_cfg' & 'pretrained_cfg_overlay' arguments:
+    kwargs.pop('pretrained_cfg', None)
+    kwargs.pop('pretrained_cfg_overlay', None)
+
+    model = VitMask(
         patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     model.default_cfg = _cfg()
